1d711e95a43401d8c52439570dbc206698cc314b
Camila Caldas <camisccb@gmail.com>
2023-03-11 10:12:39 -0300

    style: improve code writing and documentation

--- src/gcp.py
+++ src/gcp.py
@@ -1,18 +1,29 @@
+""" Execute actions in google cloud: upload file in cloud storage, detect text in document storaged and stream data extracted to BigQuery
+    
+    Functions:
+    upload_blob(bucket_name:str, source_file_name:str, destination_blob_name:str) -> str
+    detect_text_uri(uri:str) -> str
+    upload_data_bigquery(invoice_number:str, extraction_date:str, file_name:str, requester:str)
+
+"""
+
 import re
 from google.cloud import storage
 from google.cloud import vision
 from google.cloud import bigquery
 
+def upload_blob(bucket_name:str, source_file_name:str, destination_blob_name:str) -> str:
+    """ Uploads a file to the bucket
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
-    # The ID of your GCS bucket
-    #bucket_name = "your-bucket-name"
-    # The path to your file to upload
-    # source_file_name = "local/path/to/file"
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
+    Parameters:
+    bucket_name (str): The ID of your GCS bucket
+    source_file_name (str): The path to your file to upload
+    destination_blob_name (str): The ID of your GCS object
 
+    Returns:
+    gcs_path (str): URI of document in clud storage
+
+    """
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
@@ -31,12 +42,12 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         f"File {source_file_name} uploaded to {destination_blob_name}."
     )
 
+    #Create cloud storage path (URI) to unput in the detect_text_uri function
     gcs_path = f"gs://{bucket_name}/{destination_blob_name}"
     return gcs_path
 
-
-def detect_text_uri(uri):
-    """Detects text in the file located in Google Cloud Storage or on the Web."""
+def detect_text_uri(uri:str) -> str:
+    """Detects text in the file located in Google Cloud Storage"""
     client = vision.ImageAnnotatorClient()
     image = vision.Image()
     image.source.image_uri = uri
@@ -45,23 +56,20 @@ def detect_text_uri(uri):
     texts = response.text_annotations
     print('Texts:')
 
+    # For each text extracted apply the regex rule and try to find the invoice number 
     for text in texts:
         print('\n"{}"'.format(text.description))
-
+        
+        #Apply regex rule in the text extracted to found invoice number
         reg_pattern =  '\d{44}|(\d{4}\s){11}'
         text_found = re.search(reg_pattern, text.description)
 
+        # Stop after found the text
         if text_found != None:
             invoice_no = text_found.group().replace(" ", "")
             break
     
     print(invoice_no)
-    return invoice_no
-
-        #vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    #for vertex in text.bounding_poly.vertices])
-
-        #print('bounds: {}'.format(','.join(vertices)))
 
     if response.error.message:
         raise Exception(
@@ -69,21 +77,30 @@ def detect_text_uri(uri):
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
     
+    return invoice_no 
 
-def upload_data_bigquery(invoice_number, extraction_date, file_name, requester):
-    """Stream data into big query"""
+def upload_data_bigquery(invoice_number:str, extraction_date:str, file_name:str, requester:str):
+    """Stream extracted data into big query"""
     
     # Construct a BigQuery client object.
     client = bigquery.Client()
 
+    # Assign the table id that has the following structure "project-name.dataset-name.table-name"
     table_id = "invoice-reader-374817.invoice_data.EXTRACTED_DATA"
 
+    # Define row to insert. The dictionary key is the column name of table schema in bigquery
     rows_to_insert = [
         {"Extraction_date": extraction_date, "Requester": requester, "Invoice_number": invoice_number, "File":file_name}
     ]
 
-    errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
+    # Make an API request.
+    errors = client.insert_rows_json(table_id, rows_to_insert)  
     if errors == []:
         print("New rows have been added.")
     else:
         print("Encountered errors while inserting rows: {}".format(errors))
+
+
+
+
+   
